0fd89e18c92ec703d4b59a603d17789af89beb1f
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2020-09-15 12:32:57 +0200

    better offset scene names

--- analyze/spheres.sh
+++ analyze/spheres.sh
@@ -1,8 +1,8 @@
-modes="dem p4p upnp epnp"
+methods="dem p4p upnp epnp"
 refs="none vvs lm"
 noises="0 0.0001 0.00001"
 #scenes="1 2 3 4 5 6 7 8 9 10"
-scenes="2 12"
+scenes="1 1o 2 2o"
 
 dataPath=/home/olivier/Results/singularity
 if [ "$HOSTNAME" = sterne ]; then
@@ -10,13 +10,13 @@ dataPath=/home/olivier/Olivier/boulot/Results/singularity
 fi
 for s in $scenes
 do
-for m in $modes
+for method in $methods
 do
     for ref in $refs
     do
      for noise in $noises
      do
-        ../build/sphere dataPath $dataPath scene $s estim $m kill false plot false display create ref $ref noise $noise &
+        ../build/sphere dataPath $dataPath scene $s estim $method ref $ref kill false plot false noise $noise &
          done
     done
 done
